ce00f8519e447a55839301feefb20ddf8fd93966
Luis Gerardo <luis.encinas1506@gmail.com>
2021-07-13 17:26:41 -0700

    Adding dynamic cachning

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,16 +1,23 @@
 var deferredPrompt;
 
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/service-worker.js', { scope: '/help/'})
-        .then(function() {
-            console.log('Service worker registered!')
-        });
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 window.addEventListener('beforeinstallprompt', function(event) {
-    console.log('beeforeinstalledprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
 });
