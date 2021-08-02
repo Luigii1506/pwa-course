873d1a689ec446741a129662ab51353f6d594482
luigii1506 <luis.encinas1506@gmail.com>
2021-08-02 00:56:14 -0700

    Added ofline fallback page

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 
-var CACHE_STATIC_NAME = 'static-v3';
+var CACHE_STATIC_NAME = 'static-v6';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2'
 
 self.addEventListener('install', function(event) {
@@ -11,6 +11,7 @@ self.addEventListener('install', function(event) {
           cache.addAll([
             '/',
             '/index.html',
+            '/offline.html',
             '/src/js/app.js',
             '/src/js/feed.js',
             '/src/js/promise.js',
@@ -60,7 +61,10 @@ self.addEventListener('fetch', function(event) {
                 })
             })
             .catch(function(err) {
-
+              return caches.open(CACHE_STATIC_NAME)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
             });
         }
       })
